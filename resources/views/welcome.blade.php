3fbe833723cc20ab73eed4f431c9d45be43cef73
acme_fis@hotmail.com <38261020+acme204@users.noreply.github.com>
2018-04-19 12:33:19 -0500

    cambio don

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    FIS UNCP
                 </div>
 
                 <div class="links">
